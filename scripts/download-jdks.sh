4f5377274d37cdb6662813ed55d30fd7be31e530
William Kent <wjk011@gmail.com>
2024-06-27 12:36:51 -0400

    Add code to download matching OpenJDK source
    This helps maintaining GPL compliance for OpenJDK much,
    much easier, since the source code is being distributed
    with the binaries.
    
    I am downloading the source from GitHub because there is,
    stupidly, no source tarball for OpenJDK 22.0.1 available on
    the OpenJDK website. I have verified that this is the exact
    build used in the binaries through consulting the
    Contents/Home/release file in the downloaded JDK directories.

--- scripts/download-jdks.sh
+++ scripts/download-jdks.sh
@@ -23,3 +23,11 @@ if [ ! -f openjdk-${JDK_VERSION}-arm64.stamp ]; then
     curl -C - -sLo openjdk-${JDK_VERSION}-arm64.tar.gz "https://${JDK_DOWNLOAD_URL_ARM64}"
     touch openjdk-${JDK_VERSION}-arm64.stamp
 fi
+
+mkdir -p ${SRCROOT}/merged_jdk
+if [ ! -f ${SRCROOT}/merged_jdk/openjdk-${JDK_VERSION}-src.zip ]; then
+    echo Downloading OpenJDK source...
+    echo https://${JDK_DOWNLOAD_URL_SOURCE}
+    curl -C - -sLo openjdk-${JDK_VERSION}-src.zip "https://${JDK_DOWNLOAD_URL_SOURCE}"
+    mv -f openjdk-${JDK_VERSION}-src.zip ${SRCROOT}/merged_jdk
+fi
